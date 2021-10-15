040b3fa46b478e2be1ea85d49cc7e3b16a1b81b0
John Lewis <john.lewis@stratusgrid.com>
2021-10-14 22:10:07 -0400

    Add output for kms key id used to encrypt sns topic

--- -outputs.tf
+++ -outputs.tf
@@ -2,3 +2,8 @@ output "aws_config_configuration_recorder_id" {
   description = "ID of configuration recorder"
   value       = aws_config_configuration_recorder.config.id
 }
+
+output "sns_encryption_kms_key_id" {
+  description = "Id of key used to encrypt sns topic"
+  value       = local.sns_kms_key_id
+}
