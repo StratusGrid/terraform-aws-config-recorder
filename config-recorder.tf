ec870d3715de64153ffe9e909f91e405e515e994
Jheison Rodriguez <jheison.rodriguez@stratusgrid.com>
2024-07-04 15:30:33 -0500

    Add subcribers to the topic, change string policy to data type, add dynamic block for more arguments

--- config-recorder.tf
+++ config-recorder.tf
@@ -8,50 +8,77 @@ locals {
   sns_kms_key_id = var.sns_kms_key_id != "" ? var.sns_kms_key_id : data.aws_kms_key.sns_default.id
 }
 
-resource "aws_iam_role" "config" {
-  name = "aws-config-role-${data.aws_region.current.name}"
-  tags = local.common_tags
+data "aws_iam_policy_document" "assume_role" {
+  count = var.create_iam_role ? 1 : 0
+  statement {
+    effect = "Allow"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "config.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
     }
-  ]
+
+    actions = ["sts:AssumeRole"]
+  }
 }
-POLICY
+
+resource "aws_iam_role" "config" {
+  count = var.create_iam_role ? 1 : 0
+  name  = "aws-config-role-${data.aws_region.current.name}"
+  tags  = local.common_tags
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "config" {
-  role       = aws_iam_role.config.name
+  count      = var.create_iam_role ? 1 : 0
+  provider   = aws
+  role       = aws_iam_role.config[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
 
 resource "aws_config_configuration_recorder" "config" {
-  name     = "aws-config-recorder"
-  role_arn = aws_iam_role.config.arn
+  name = "aws-config-recorder"
+  # required argument, an arn of a valid role should be pass if the role is not created
+  role_arn = var.create_iam_role ? aws_iam_role.config[0].arn : var.iam_role_arn
 
   recording_group {
     all_supported                 = true
     include_global_resource_types = var.include_global_resource_types
   }
+
+  dynamic "recording_mode" {
+    for_each = var.recording_mode != null ? [1] : []
+    content {
+      recording_frequency = var.recording_mode.recording_frequency
+      dynamic "recording_mode_override" {
+        for_each = var.recording_mode.recording_mode_override != null ? [1] : []
+        content {
+          description         = var.recording_mode.recording_mode_override.description
+          resource_types      = var.recording_mode.recording_mode_override.resource_types
+          recording_frequency = var.recording_mode.recording_mode_override.recording_frequency
+        }
+      }
+    }
+  }
 }
 
 resource "aws_config_delivery_channel" "config" {
+  provider       = aws
   name           = "aws-config-delivery-channel"
   s3_bucket_name = var.log_bucket_id
-  s3_key_prefix  = "config"
-  sns_topic_arn  = aws_sns_topic.aws_config_stream.arn
+  # To maintain bakcward compatibility with the previous version
+  s3_key_prefix = var.s3_key_prefix != null ? var.s3_key_prefix : "config"
+  sns_topic_arn = var.create_sns_topic == true ? aws_sns_topic.aws_config_stream[0].arn : ""
 
-  snapshot_delivery_properties {
-    delivery_frequency = var.snapshot_delivery_frequency
+  dynamic "snapshot_delivery_properties" {
+    # To maintain bakcward compatibility with the previous version the default value of snapshot_delivery_frequency where leave as is
+    for_each = var.snapshot_delivery_frequency != null && var.snapshot_delivery_frequency != "" ? [1] : []
+
+    content {
+      delivery_frequency = var.snapshot_delivery_frequency
+    }
+
   }
 
   depends_on = [aws_config_configuration_recorder.config]
@@ -66,12 +93,15 @@ resource "aws_config_configuration_recorder_status" "config" {
 
 #tfsec:ignore:aws-sns-enable-topic-encryption -- Ignores error on Turning on SNS Topic encryption.
 resource "aws_sns_topic" "aws_config_stream" {
+  count = var.create_sns_topic ? 1 : 0
+
   name              = "aws-config-stream-${data.aws_region.current.name}"
   kms_master_key_id = local.sns_kms_key_id
   tags              = local.common_tags
 }
 
 data "aws_iam_policy_document" "config_sns" {
+  count = var.create_sns_topic ? 1 : 0
   statement {
     actions = [
       "sns:Publish",
@@ -83,7 +113,7 @@ data "aws_iam_policy_document" "config_sns" {
       type = "Service"
     }
     resources = [
-      aws_sns_topic.aws_config_stream.arn,
+      aws_sns_topic.aws_config_stream[0].arn,
     ]
     sid = "ConfigSNSPolicy"
   }
@@ -106,13 +136,33 @@ data "aws_iam_policy_document" "config_sns" {
       type = "AWS"
     }
     resources = [
-      aws_sns_topic.aws_config_stream.arn,
+      aws_sns_topic.aws_config_stream[0].arn,
     ]
     sid = "DenyUnsecuredTransport"
   }
 }
 
 resource "aws_sns_topic_policy" "config" {
-  arn    = aws_sns_topic.aws_config_stream.arn
-  policy = data.aws_iam_policy_document.config_sns.json
+  count  = var.create_sns_topic ? 1 : 0
+  arn    = aws_sns_topic.aws_config_stream[0].arn
+  policy = data.aws_iam_policy_document.config_sns[0].json
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  for_each = local.conditional_subscribers
+
+  topic_arn = aws_sns_topic.aws_config_stream[0].arn
+
+
+  protocol               = lookup(each.value, "protocol")
+  endpoint               = lookup(each.value, "endpoint")
+  endpoint_auto_confirms = lookup(each.value, "endpoint_auto_confirms", false)
+  raw_message_delivery   = lookup(each.value, "raw_message_delivery", false)
+
+}
+
+
+locals {
+  # Conditionally create a map of subscribers if the topic is created
+  conditional_subscribers = var.create_sns_topic ? { for key, value in var.subscribers : key => value } : {}
 }
--- inputs.tf
+++ inputs.tf
@@ -12,6 +12,43 @@
 #   default = ""
 # }
 
+variable "create_iam_role" {
+  description = "Flag to indicate whether an IAM Role should be created to grant the proper permissions for AWS Config"
+  type        = bool
+  default     = false
+}
+
+variable "create_sns_topic" {
+  description = <<-DOC
+    Flag to indicate whether an SNS topic should be created for notifications
+    If you want to send findings to a new SNS topic, set this to true and provide a valid configuration for subscribers
+    If you are using this module to set multiple accounts and regions, only enable the SNS topic in the aggregator account and region.
+  DOC
+  type        = bool
+  default     = true
+}
+
+variable "iam_role_arn" {
+  description = <<-DOC
+    The ARN for an IAM Role AWS Config uses to make read or write requests to the delivery channel and to describe the
+    AWS resources associated with the account. This is only used if create_iam_role is false.
+
+    If you want to use an existing IAM Role, set the value of this to the ARN of the existing topic and set
+    create_iam_role to false.
+
+    See the AWS Docs for further information:
+    http://docs.aws.amazon.com/config/latest/developerguide/iamrole-permissions.html
+  DOC
+  default     = null
+  type        = string
+}
+
+variable "include_global_resource_types" {
+  description = "True/False to add global resources to config. Default is false"
+  type        = string
+  default     = false
+}
+
 variable "input_tags" {
   description = "Map of tags to apply to resources"
   type        = map(any)
@@ -26,20 +63,93 @@ variable "log_bucket_id" {
   type        = string
 }
 
+variable "recording_mode" {
+  description = <<-DOC
+    The mode for AWS Config to record configuration changes. 
+
+    recording_frequency:
+      The frequency with which AWS Config records configuration changes (service defaults to CONTINUOUS).
+      - CONTINUOUS
+      - DAILY
+
+      You can also override the recording frequency for specific resource types.
+    recording_mode_override:
+      description:
+        A description for the override.
+      resource_types:
+        A list of resource types for which AWS Config records configuration changes. For example, AWS::EC2::Instance.
+        Refer to: https://docs.aws.amazon.com/config/latest/APIReference/API_RecordingModeOverride.html
+      recording_frequency:
+        The frequency with which AWS Config records configuration changes for the specified resource types.
+        - CONTINUOUS
+        - DAILY
+
+    /*
+    recording_mode = {
+      recording_frequency = "DAILY"
+      recording_mode_override = {
+        description         = "Override for specific resource types"
+        resource_types      = ["AWS::EC2::Instance"]
+        recording_frequency = "CONTINUOUS"
+      }
+    }
+    */
+  DOC
+  type = object({
+    recording_frequency = string
+    recording_mode_override = optional(object({
+      description         = string
+      resource_types      = list(string)
+      recording_frequency = string
+    }))
+  })
+  default = null
+}
+
+variable "s3_key_prefix" {
+  type        = string
+  description = <<-DOC
+    The prefix for AWS Config objects stored in the the S3 bucket. If this variable is set to null, the default, no
+    prefix will be used.
+
+    Examples:
+
+    with prefix:    {S3_BUCKET NAME}:/{S3_KEY_PREFIX}/AWSLogs/{ACCOUNT_ID}/Config/*.
+    without prefix: {S3_BUCKET NAME}:/AWSLogs/{ACCOUNT_ID}/Config/*.
+  DOC
+  default     = null
+}
+
 variable "snapshot_delivery_frequency" {
   description = "Frequency which AWS Config snapshots the configuration"
   type        = string
   default     = "Three_Hours"
 }
 
-variable "include_global_resource_types" {
-  description = "True/False to add global resources to config. Default is false"
-  type        = string
-  default     = false
-}
-
 variable "sns_kms_key_id" {
   description = "KMS key id for encrypting cloudtrail config recorder stream sns topic. If left empty uses SNS default AWS managed key."
   type        = string
   default     = ""
+}
+
+variable "subscribers" {
+  type        = map(any)
+  description = <<-DOC
+    A map of subscription configurations for SNS topics
+
+    For more information, see:
+    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic_subscription#argument-reference
+
+    protocol:
+      The protocol to use. The possible values for this are: sqs, sms, lambda, application. (http or https are partially
+      supported, see link) (email is an option but is unsupported in terraform, see link).
+    endpoint:
+      The endpoint to send data to, the contents will vary with the protocol. (see link for more information)
+    endpoint_auto_confirms (Optional):
+      Boolean indicating whether the end point is capable of auto confirming subscription e.g., PagerDuty. Default is
+      false
+    raw_message_delivery (Optional):
+      Boolean indicating whether or not to enable raw message delivery (the original message is directly passed, not wrapped in JSON with the original message in the message property). Default is false.
+  DOC
+  default     = {}
 }
